d1bf44f5ae24b138a60ac807763a31f44eb2395c
Kai Cong <crizzs_2@hotmail.com>
2018-09-26 17:53:40 +0800

    Added basic functions

--- internal/basic_func/basic_funcs.go
+++ internal/basic_func/basic_funcs.go
@@ -1,9 +1,28 @@
 package syntheticsv
 
+import(
+	"errors"
+)
 /*******
 Merge values on a single row with delimiter
 ********/
-func MergeValues(fields []string,delim string) string{
+func MergeValues(fields []string, delim string) string{
 	MoveToNextLine()
+	//Split the Line into Array
+	anotherLineArr := SplitLine(csvFileStore.Text())
+
+	i := 0
+
+	for i < len(anotherLineArr) {
+		
+	}
+
 	return ""
+}
+
+/*******
+Gets Value from String Array
+*******/
+func RetrieveValue(allValues []string, fieldName string) (string,error){
+	return "" ,_
 }
--- internal/basic_func/basic_funcs_test.go
+++ internal/basic_func/basic_funcs_test.go
@@ -8,12 +8,18 @@ import(
 func TestMergeValues(t *testing.T){
 	//Reset the settings to default
 	SetAllBasicSettings()
-	mergedInfo := MergeValues([]string{"iata_code","country"},"-")
+	mergedInfo := MergeValues([]string{"iata_code","country"},"|")
 	//Test the return value
 	assert.Equal(t,mergedInfo, "AAAFrench Polynesia", "First and Last value should merge.")
 }
 
-
+func TestRetrieveVals(t *testing.T){
+	MoveToNextLine()
+	anotherLineArr := SplitLine(csvFileStore.Text())
+	
+	//Retrieve Value based on field
+	val, err := RetrieveValue(anotherLineArr,"country")
+}
 
 //Function to evoke all basic settings for Unit Test purpose
 func SetAllBasicSettings() {
